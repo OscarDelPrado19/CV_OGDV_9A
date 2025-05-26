cc1958df1762ea6aacbc3f3a29b5040dfe4e6470
OscarDelPrado19 <20223tn091@utez.edu.mx>
2025-05-26 17:44:28 -0600

    [First commit] subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cvogdv9a/Cvogdv9aApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvogdv9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvogdv9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvogdv9aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvogdv9a/Cvogdv9aApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvogdv9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvogdv9aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
